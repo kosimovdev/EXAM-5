6695512e2e5ec65f35b9d6f77eca4fb9fb06c8ae
kosimovdev <kosimov.oyatillo@gmail.com>
2023-09-26 22:03:00 +0500

    header section is finished

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header/index.jsx";
+import Layout from "./components/Layout/index.jsx";
+
+const App = () => {
+  return (
+    <>
+        <Header/>
+        <Layout/>
+    </>
+  );
+};
+
+export default App;
